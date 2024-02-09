8659827bec85f89c5829693d04baeff91b100984
june lee <melodysdreamj@gmail.com>
2024-02-09 13:59:28 +0900

    `

--- lib/app/_/_/interaction/view/page/page/page/_/vm/_/cycle/init.dart
+++ lib/app/_/_/interaction/view/page/page/page/_/vm/_/cycle/init.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../../../../../../../../util/start_app.dart';
-import '../../../page.ready_before_move/_/config/params.dart';
+import '../../../page/_/config/params.dart';
 import '../util.dart';
 import '../vm.dart';
 
--- lib/app/_/_/interaction/view/page/page/page/_/vm/_/util.dart
+++ lib/app/_/_/interaction/view/page/page/page/_/vm/_/util.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import '../../../../../../../../../../../util/start_app.dart';
-import '../../page.ready_before_move/_/config/params.dart';
+import '../../page/_/config/params.dart';
 import 'cycle/delete.dart';
 import 'cycle/init.dart';
 import 'vm.dart';
